bbabbad905bf05c561d9726c104f3c002cc57cb4
Jedi <jedidiahbasil@gmail.com>
2022-07-25 21:53:43 +0100

    check if an element is set in the localStorage withthe name movie and then assign it to the array called movie

--- js/upload.js
+++ js/upload.js
@@ -5,3 +5,6 @@ const title = document.getElementById('title');
 const description = document.getElementById('description');
 
 let movie = [];
+if(localStorage.getItem('movie')){
+    movie = JSON.parse(localStorage.getItem('movie'));
+}
