914d2e9ddc24a47a715d3ce0d3c1d05b79f56d4b
Jedi <jedidiahbasil@gmail.com>
2022-07-25 22:21:57 +0100

    created a for in to go through the array and display all the items in the html file

--- js/displayItems.js
+++ js/displayItems.js
@@ -1,1 +1,22 @@
 const root = document.getElementById("root");
+let movie = [];
+
+if (localStorage.getItem("movie")) {
+    movie = JSON.parse(localStorage.getItem("movie"));
+}
+
+const setPath = (img)=>{
+    return `./img/${img}`
+}
+
+for (const file in movie) {
+    `
+    ${ root.innerHTML+= `
+   <div class="card" key="${movie[file].id}">
+   <h1>${movie[file].title}</h1>
+       <img src="${setPath(movie[file].img)}">
+       <p>${movie[file].description}</p></div>`}` 
+
+       console.log(movie[file].description);
+
+}
