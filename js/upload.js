2681d8b5e9152c22ffeacce4a9f7047b73f06854
Jedi <jedidiahbasil@gmail.com>
2022-07-25 21:49:25 +0100

    gotten the elements from the html file

---
+++ js/upload.js
@@ -0,0 +1,5 @@
+const fileInput = document.getElementById('fileInput');
+const movieForm = document.getElementById('movieForm');
+const uploadImg = document.getElementById('uploadImg');
+const title = document.getElementById('title');
+const description = document.getElementById('description');
