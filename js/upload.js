e31c8e73b955c24ff954921d185aca166057dfd9
Jedi <jedidiahbasil@gmail.com>
2022-07-25 21:51:02 +0100

    create an array to hold each element to be set in the localStorage

--- js/upload.js
+++ js/upload.js
@@ -3,3 +3,5 @@ const movieForm = document.getElementById('movieForm');
 const uploadImg = document.getElementById('uploadImg');
 const title = document.getElementById('title');
 const description = document.getElementById('description');
+
+let movie = [];
